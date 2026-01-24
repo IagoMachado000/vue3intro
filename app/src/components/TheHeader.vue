4cc7e86ec5b2434335625e168e469991107f0120
Iago Machado <iago.machado000@gmail.com>
2026-01-24 16:09:02 -0300

    Componentes de arquivo único SFC

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<!-- HTML -->
+<template>
+    <header class="header">Header</header>
+</template>
+
+<!-- JAVASCRIPT -->
+<script>
+export default {};
+</script>
+
+<!-- ESTILO -->
+<style>
+.header {
+    color: #fff;
+    background: gray;
+}
+</style>
