2ecb8a9ee900a3e4c1e2089ae171cab513caffb0
Iago Machado <iago.machado000@gmail.com>
2026-01-25 12:51:50 -0300

    #5 Instruções de loops (diretiva v-for)

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,11 +1,40 @@
 <!-- HTML -->
 <template>
-    <header class="header">Header</header>
+    <header class="header">
+        <nav>
+            <ul class="menu">
+                <li v-for="item in menu" :key="item.name">
+                    <a :href="item.uri">
+                        {{ item.name }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <!-- JAVASCRIPT -->
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            menu: [
+                {
+                    name: "home",
+                    uri: "/",
+                },
+                {
+                    name: "about",
+                    uri: "/about",
+                },
+                {
+                    name: "contact",
+                    uri: "/contact",
+                },
+            ],
+        };
+    },
+};
 </script>
 
 <!-- ESTILO -->
@@ -13,5 +42,16 @@ export default {};
 .header {
     color: #fff;
     background: gray;
+}
+.header .menu {
+    list-style-type: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+}
+.header .menu a {
+    color: inherit;
+    text-decoration: none;
 }
 </style>
