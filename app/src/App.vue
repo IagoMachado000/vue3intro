6142757a802a79950bd55a089712003afe03909a
Iago Machado <iago.machado000@gmail.com>
2026-01-26 08:28:54 -0300

    #8 Ligação de dados bidirecional (diretiva v model)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,37 +1,87 @@
 <template>
-    <TheHeader v-if="showHeader" />
+    <div>
+        <div>
+            One-way data binding
+            <br />
+            Two-way data binding
+            <br />
+            v-model -> formulários
+        </div>
 
-    <div v-show="showName">
-        Nome: {{ firstName }}
-        <br />
-        Sobrenome: {{ lastName }}
+        <br /><br />
+
+        <div>
+            <label>Nome</label>
+            <br />
+            <input type="text" v-model="name" />
+            <br />
+            {{ name }}
+        </div>
+
+        <br /><br />
+
+        <div>
+            <label>Sports</label>
+            <br />
+            <select v-model="sports">
+                <option value="">Escolha</option>
+                <option value="futebol">Futebol</option>
+                <option value="skate">Skate</option>
+                <option value="tenis">Tênis</option>
+            </select>
+            <br />
+            {{ sports }}
+        </div>
+
+        <br /><br />
+
+        <div>
+            <div>
+                <label>Não</label>
+                <input type="radio" value="Não" v-model="newsletter" />
+            </div>
+            <div>
+                <label>Sim</label>
+                <input type="radio" value="Sim" v-model="newsletter" />
+            </div>
+            {{ newsletter }}
+        </div>
+
+        <br /><br />
+
+        <div>
+            <div>
+                <label>Contrato</label>
+                <br />
+                <input type="checkbox" v-model="contracts" />
+                Aceita nossos termos ...
+            </div>
+            {{ contracts }}
+        </div>
+
+        <br /><br />
+
+        <div>
+            <label>Cores que você mais gosta</label>
+            <br />
+            <input type="checkbox" value="Azul" v-model="colors" /> Azul
+            <input type="checkbox" value="Amarelo" v-model="colors" /> Amarelo
+            <br />
+            {{ colors }}
+        </div>
     </div>
-
-    <div v-if="accessLevel === 'admin'">Admin</div>
-    <div v-else-if="accessLevel === 'marketing'">Marketing</div>
-    <div v-else>Usuário normal</div>
-
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheHeader from "./components/TheHeader.vue";
-
 export default {
     name: "App",
-    components: {
-        HelloWorld,
-        TheHeader,
-    },
     data() {
         return {
-            showHeader: true,
-            firstName: "John",
-            lastName: "Doe",
-            showName: false,
-            accessLevel: "admin",
+            name: "",
+            sports: "",
+            newsletter: "Não",
+            contracts: false,
+            colors: [],
         };
     },
 };
