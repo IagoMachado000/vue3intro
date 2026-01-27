6aa7c5e4493c573ebb8f180ad18a78bde1c32b53
Iago Machado <iago.machado000@gmail.com>
2026-01-27 12:11:20 -0300

    #17 Introdução ao Vue Router parte 1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,125 +1,21 @@
 <template>
-    <div>
-        <TheHeader v-if="showHeader">
-            <!-- <template v-slot:title> Home </template> -->
-
-            <template v-slot:description>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </template>
-
-            Home
-        </TheHeader>
-        <h1>Hello World</h1>
-        {{ name }}
-        <br />
-        <button @click="showHeader = !showHeader">
-            Ativar e desativar header
-        </button>
-        <br />
-        <input type="text" v-model="name" />
-
-        <br /><br /><br />
-
-        <div class="card text-small">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-            reiciendis sapiente repellat aliquid, placeat accusantium tempora.
-            Ab minus eius nesciunt quidem est dignissimos, numquam beatae. Atque
-            accusamus tempore culpa dolorum?
-        </div>
-
-        <BaseCard></BaseCard>
-
-        <br /><br /><br />
-
-        <BaseAlert v-if="showAlert" :variant="variant" @close="onClose()">
-            <small>{{ text }}</small>
-        </BaseAlert>
-    </div>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link :to="rotaDinamica">Serviços</router-link>
+    </nav>
+    <router-view />
 </template>
 
 <script>
-import BaseAlert from "./components/BaseAlert.vue";
-import BaseCard from "./components/BaseCard.vue";
-import TheHeader from "./components/TheHeader.vue";
-
 export default {
-    name: "App",
-    components: { TheHeader, BaseCard, BaseAlert },
     data() {
         return {
-            name: "John Doe",
-            showHeader: true,
-            variant: "danger",
-            text: "Houve um erro no envio do seu formulário",
-            showAlert: true,
+            rotaDinamica: { name: "servicos" },
         };
-    },
-
-    // Criação
-    // Preparar o componente
-    // Aja, inicializar algumas variáveis
-    // Não tem acesso ao template (DOM)
-
-    // Montagem
-    // Inicializar um lib externa (new Lib())
-    // Precisa de acesso ao template (DOM)
-    // Tem acesso ao template (DOM)
-
-    // Atualização
-    // Debug
-    // Update
-
-    // Desmontagem
-    // Remover tudo que for necessário para liberar memória (lib->destroy())
-
-    // HOOKS
-    beforeUpdate() {
-        console.log("beforeUpdate");
-    },
-    updated() {
-        console.log("updated");
-    },
-    // beforeCreate() {
-    //     console.log("beforeCreate");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    // created() {
-    //     console.log("created");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    // beforeMount() {
-    //     console.log("beforeMount");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    // mounted() {
-    //     console.log("mounted");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    // beforeUnmount() {
-    //     console.log("beforeUnmount");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    // unmounted() {
-    //     console.log("unmounted");
-    //     console.log("Estado:", this.name);
-    //     console.log("DOM:", this.$el);
-    // },
-    watch: {},
-    computed: {},
-    methods: {
-        onClose() {
-            this.showAlert = false;
-            console.log("on close");
-        },
     },
 };
 </script>
-
 <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -127,6 +23,18 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+}
+
+nav {
+    padding: 30px;
+}
+
+nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+    color: #42b983;
 }
 </style>
