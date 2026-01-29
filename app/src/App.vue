51ac389326f3f782701dbf58b1368b2c6022c1bc
Iago Machado <iago.machado000@gmail.com>
2026-01-29 15:56:03 -0300

    #25 Introdução a composition API Reatividade

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ name }}</p>
+    <p>{{ user.first_name }} - {{ user.last_name }}</p>
+    <p>{{ admin.first_name }} - {{ admin.last_name }}</p>
+    <img alt="Vue logo" src="./assets/logo.png" @click="changeName" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+    setup() {
+        const user = reactive({
+            first_name: "John",
+            last_name: "Doe",
+        });
+
+        const admin = ref({
+            first_name: "Admin",
+            last_name: "Master",
+        });
+
+        let name = "Fulano";
+
+        const changeName = () => {
+            alert("alterou");
+            user.first_name = "Jane";
+            admin.value.first_name = "Ciclano";
+        };
+
+        return {
+            user,
+            admin,
+            name,
+            changeName,
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
