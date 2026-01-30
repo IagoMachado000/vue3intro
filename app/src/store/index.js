e3dcec7c09c9bb167a2a0c8f14cf30797873b058
Iago Machado <iago.machado000@gmail.com>
2026-01-30 15:48:02 -0300

    #33 Projeto aula 05

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -11,7 +11,20 @@ export default createStore({
             state.todos = payload;
         },
         storeTodo(state, payload) {
-            state.todos.unshift(payload);
+            const index = state.todos.findIndex(
+                (todo) => todo.id === payload.id,
+            );
+            if (index >= 0) {
+                state.todos.splice(index, 1, payload);
+            } else {
+                state.todos.unshift(payload);
+            }
+        },
+        deleteTodo(state, id) {
+            const index = state.todos.findIndex((todo) => todo.id === id);
+            if (index >= 0) {
+                state.todos.splice(index, 1);
+            }
         },
     },
     actions: {
@@ -35,8 +48,19 @@ export default createStore({
                     commit("storeTodo", response.data);
                 });
         },
-        updateTodo(context, { id, data }) {
-            return axios.put(`http://localhost:3000/todos/${id}`, data);
+        updateTodo({ commit }, { id, data }) {
+            return axios
+                .put(`http://localhost:3000/todos/${id}`, data)
+                .then((response) => {
+                    commit("storeTodo", response.data);
+                });
+        },
+        deleteTodo({ commit }, id) {
+            return axios
+                .delete(`http://localhost:3000/todos/${id}`)
+                .then(() => {
+                    commit("deleteTodo", id);
+                });
         },
     },
     modules: {},
